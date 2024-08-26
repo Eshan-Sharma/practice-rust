df871aed4bf5cb44fb63d4120937d0144b12b52e
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-08-26 10:32:03 +0530

    Option

--- second-project/src/main.rs
+++ second-project/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
+    //Options
+    println!("{:?}", find_square_root(3.0));
+}
+
+fn find_square_root(number: f64) -> Option<f64> {
+    if number >= 0.0 {
+        Some(number.sqrt())
+    } else {
+        None
+    }
 }
