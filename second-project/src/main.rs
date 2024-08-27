1fe47aa4696dbccfd3fe302ea4a1cd3ae4462fc9
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-08-27 08:26:30 +0530

    Option with match

--- second-project/src/main.rs
+++ second-project/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     println!("Hello, world!");
     //Options
     println!("{:?}", find_square_root(3.0));
+    let number = -4.0;
+    let square_root = find_square_root(number);
+    match square_root {
+        Some(_value) => println!("Inside some"),
+        None => println!("Inside none"),
+    }
 }
 
 fn find_square_root(number: f64) -> Option<f64> {
